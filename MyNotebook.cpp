98c348954cf872af237991987fed7e06af8b623f
chand <shaikchand8832@gmail.com>
2022-04-16 00:16:09 +0530

    added quote page

--- MyNotebook.cpp
+++ MyNotebook.cpp
@@ -16,20 +16,21 @@
 #include <cstdlib>
 void gotoxy(int, int);
 bool checkfile();
-void ShowConsoleCursor(bool);
+void showCursor(bool);
+void erasePrint(int, int, int, int);
+bool showMessage(std::string, std::string, bool);
 void notebook();
 void welcome();
-bool showMessage(std::string, std::string, bool);
 void change_name();
 void main_menu();
-std::vector<std::string> randomQuote;
+std::vector<std::string> randomQuote;                                                 //threee varible for manipulating quotes globally
 std::string oneQuote;
 bool quoteMode;
 
 class IncryptDecrypt
 {
 private:
-    int secretkey[18] = {21, 13, 8, 5, 3, 2, 1, 1, 0, 2, 3, 5, 7, 11, 13, 17, 19};
+    int secretkey[18] = {21, 13, 8, 5, 3, 2, 1, 1, 0, 2, 3, 5, 7, 11, 13, 17, 19};   //secret key to incrypt and decrypt
 
 public:
     std::string incrypt(std::string);
@@ -48,6 +49,14 @@ public:
     void change_password();
     void security_Question(bool);
 };
+class Quotes : public IncryptDecrypt
+{
+public:
+    void quote_menu();
+    void readQuotes();
+    void quote_viewMenu();
+    void changeQuoteMode();
+};
 int main()
 {
     system("title MY NOTEBOOK");
@@ -62,9 +71,17 @@ int main()
         bool loginsuccess = p.login();
         if (loginsuccess)
         {
-            ShowConsoleCursor(false);
+            showCursor(false);
             welcome();
-            ShowConsoleCursor(true);
+            showCursor(true);
+            std::ifstream fquotemode("files/Quotes/quotemode.txt");                      //get quote mode because in notebook 
+            if (fquotemode.is_open())                                                    //for random quote quote mode need to be true
+            {
+                fquotemode >> quoteMode;                                    
+            }
+            fquotemode.close();
+            Quotes a;
+            a.readQuotes();                                                                //loading quotes
             main_menu();
         }
         else
@@ -73,24 +90,35 @@ int main()
         }
     }
 }
-void gotoxy(int x, int y)
+void gotoxy(int x, int y)                                                                   //for setting cursor position on console
 {
     COORD pos;
     pos.X = x;
     pos.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
-void ShowConsoleCursor(bool visibility)
+void showCursor(bool visibility)                                                           //to show or hide cursor
 {
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
 
     CONSOLE_CURSOR_INFO cursorInfo;
 
     GetConsoleCursorInfo(out, &cursorInfo);
-    cursorInfo.bVisible = visibility;
+    cursorInfo.bVisible = visibility;                                                       //true to show | false to hide cursor
     SetConsoleCursorInfo(out, &cursorInfo);
 }
-bool showMessage(std::string msg, std::string colorcode, bool option)
+void erasePrint(int x1, int x2, int y1, int y2)                                            //erase from one cordinate to another cordinate
+{
+    for (int i = y1; i <= y2; i++)
+    {
+        for (int j = x1; j <= x2; j++)
+        {
+            gotoxy(j, i);
+            std::cout << " ";
+        }
+    }
+}
+bool showMessage(std::string msg, std::string colorcode, bool option)                     //showing some message and taking yes or no
 {
     system("cls");
     gotoxy(50, 10);
@@ -131,13 +159,13 @@ bool showMessage(std::string msg, std::string colorcode, bool option)
     system("color 07");
     return false;
 }
-void notebook()
+void notebook()                                                                                 // display on top of page 
 {
     gotoxy(49, 1);
     std::cout << "MY NOTEBOOK";
-    std::string displayQuote = " ";
+    std::string displayQuote = "   ";
     int n;
-    if (quoteMode)
+    if (quoteMode)                                                                              // selecting random quote
     {
         srand(time(0));
         if (randomQuote.size() > 0)
@@ -150,18 +178,18 @@ void notebook()
     {
         displayQuote = oneQuote;
     }
-    int i = displayQuote.length();
+    int i = displayQuote.length();  
 
-    i = 50 - (int)((i - 10) / 2);
+    i = 50 - (int)((i - 10) / 2);                                                                 // based on length quote position shift
     if (i >= 100)
         i = 0;
     gotoxy(i, 3);
-    std::cout << "\"" << displayQuote << "\"";
+    std::cout << displayQuote;
     time_t now = time(0);
     gotoxy(80, 7);
     std::cout << ctime(&now);
 }
-void welcome()
+void welcome()                                                                                  // welcome message if name entered it will show
 {
     system("cls");
     system("color 07");
@@ -179,9 +207,9 @@ void welcome()
         std::getline(fname, name);
         if (name.empty())
         {
-            ShowConsoleCursor(true);
+            showCursor(true);
             gotoxy(45, 10);
-            std::cout << "Enter your name: ";
+            std::cout << "Enter your name: ";                                                       //if name not entered or not set here take
             std::getline(std::cin, name);
             fname.close();
         }
@@ -195,9 +223,9 @@ void welcome()
         Sleep(1000);
     }
 }
-bool checkfile()
+bool checkfile()                                                                            //it will check file folder exist or not
 {
-    struct stat buffer;
+    struct stat buffer;                                                                    // if file folder not existed then it will create
     char s[] = "files";
     if (stat(s, &buffer) == -1)
     {
@@ -325,7 +353,7 @@ std::string Privacy::input_password(std::string display, bool showpass)
 }
 bool Privacy::login()
 {
-    ShowConsoleCursor(false);
+    showCursor(false);
     system("cls");
     gotoxy(45, 11);
     std::cout << " +================+";
@@ -397,7 +425,7 @@ bool Privacy::login()
                                 system("color 07");
                                 gotoxy(34, 10);
                                 std::cout << "Enter your favourite teacher name: ";
-                                ShowConsoleCursor(true);
+                                showCursor(true);
                                 std::getline(std::cin, e_teachername);
                                 if (e_teachername == r_teachername)
                                 {
@@ -427,7 +455,7 @@ bool Privacy::login()
             }
         }
     }
-    ShowConsoleCursor(true);
+    showCursor(true);
     system("color 07");
     return true;
 }
@@ -530,7 +558,7 @@ void change_name()
     std::cout << "+===========+";
     gotoxy(30, 10);
     std::cout << "Enter your name: ";
-    ShowConsoleCursor(true);
+    showCursor(true);
     std::string name;
     std::getline(std::cin, name);
     fname << name;
@@ -540,9 +568,352 @@ void change_name()
     system("pause");
     fname.close();
 }
+void Quotes::readQuotes()
+{
+    std::ifstream fquote("files/Quotes/quotes.txt");
+    std::ifstream fquotemode("files/Quotes/quotemode.txt");
+    std::ifstream fonequote("files/Quotes/onequote.txt");
+
+    if (!fquote.is_open() || !fquotemode.is_open() || !fonequote.is_open())
+    {
+        return;
+    }
+    else
+    {
+        char mode;
+        std::string tempquote;
+        fquotemode >> mode;
+        switch (mode)
+        {
+        case '1':
+        {
+            std::getline(fonequote, tempquote);
+            if (tempquote.length() != 0)
+            {
+                tempquote = decrypt(tempquote);
+                oneQuote = tempquote;
+            }
+            else
+            {
+                oneQuote = " ";
+            }
+            break;
+        }
+        case '2':
+        {
+            randomQuote.clear();
+            while (!fquote.eof())
+            {
+
+                std::getline(fquote, tempquote);
+                tempquote = decrypt(tempquote);
+                if (!tempquote.empty())
+                    randomQuote.push_back(tempquote);
+            }
+
+            break;
+        }
+        }
+    }
+
+    fquote.close();
+    fonequote.close();
+    fquotemode.close();
+}
+void Quotes::changeQuoteMode()
+{
+    system("cls");
+    notebook();
+    std::string getQuote;
+    bool selectchoice = true;
+    gotoxy(50, 6);
+    std::cout << "Quote mode";
+    gotoxy(49, 7);
+    std::cout << "+==========+";
+    gotoxy(48, 9);
+    std::cout << "1.One quote";
+    gotoxy(48, 10);
+    std::cout << "2.Previous One quote";
+    gotoxy(48, 11);
+    std::cout << "3.Random quote";
+    gotoxy(48, 12);
+    std::cout << "4.Return quote menu";
+    gotoxy(30, 13);
+    std::cout << "Select your option:";
+    while (selectchoice)
+    {
+        gotoxy(50, 13);
+        char choice = getch();
+        switch (choice)
+        {
+        case '1':
+        {
+
+            std::ofstream fquotemode("files/Quotes/quotemode.txt", std::ios::trunc);
+            std::string templine;
+            fquotemode << "1";
+            fquotemode.close();
+            while (true)
+            {
+                system("cls");
+                gotoxy(15, 10);
+                std::cout << "Enter Quote: ";
+                std::getline(std::cin, templine);
+
+                if (templine.length() > 150)
+                {
+                    gotoxy(30, 14);
+                    std::cout << "Quote length over 150 characters please Enter less than 150 characters";
+                    gotoxy(45, 16);
+                    system("pause");
+                    continue;
+                }
+                std::ofstream fquote("files/Quotes/onequote.txt", std::ios::trunc);
+                if (templine.length() != 0)
+                {
+                    oneQuote = templine;
+                    templine = incrypt(templine);
+                    fquote << templine;
+                    quoteMode = false;
+                }
+                fquote.close();
+                gotoxy(45, 15);
+                std::cout << "Succefully changed quote mode";
+                gotoxy(45, 17);
+                system("pause");
+                break;
+            }
+            readQuotes();
+            selectchoice = false;
+            break;
+        }
+        case '2':
+        {
+            std::ofstream fquotemode("files/Quotes/quotemode.txt", std::ios::trunc);
+            quoteMode = false;
+            fquotemode << "1";
+            selectchoice = false;
+            fquotemode.close();
+            readQuotes();
+            break;
+        }
+        case '3':
+        {
+            std::ofstream fquotemode("files/Quotes/quotemode.txt", std::ios::trunc);
+            quoteMode = true;
+            fquotemode << "2";
+            selectchoice = false;
+            fquotemode.close();
+            readQuotes();
+            break;
+        }
+        case '4':
+        {
+            selectchoice = false;
+            break;
+        }
+        default:
+        {
+            break;
+        }
+        }
+    }
+
+    return;
+}
+void Quotes::quote_viewMenu()
+{
+    char choice;
+    bool selectchoice = true;
+    system("cls");
+    notebook();
+    gotoxy(50, 6);
+    std::cout << "View Menu";
+    gotoxy(49, 7);
+    std::cout << "+===========+";
+    gotoxy(48, 9);
+    std::cout << "1.Add quote";
+    gotoxy(48, 10);
+    std::cout << "2.Delete quote";
+    gotoxy(48, 11);
+    std::cout << "3.Return quote menu";
+    {
+        gotoxy(1, 17);
+        std::cout << "_____________________________________________________________________________________";
+        for (int i = 0; i < randomQuote.size(); i++)
+        {
+            gotoxy(1, 19 + i);
+            std::cout << i + 1 << "." << randomQuote.at(i);
+        }
+    }
+    while (selectchoice)
+    {
+        gotoxy(30, 13);
+        std::cout << "Select your option: ";
+        gotoxy(50, 13);
+        choice = getch();
+        switch (choice)
+        {
+        case '1':
+        {
+            while (true)
+            {
+                system("cls");
+                gotoxy(15, 13);
+                std::cout << "Enter Quote: ";
+                std::string line;
+                std::getline(std::cin, line);
+                if (line.empty())
+                {
+                    gotoxy(50, 15);
+                    std::cout << "unable add empty quote";
+                }
+                else
+                {
+                    randomQuote.push_back(line);
+                }
+                gotoxy(40, 16);
+                std::cout << "Do you want to add another quote(Y/N): ";
+                char quotechoice = getch();
+                if (quotechoice == 'n' || quotechoice == 'N')
+                {
+                    break;
+                }
+            }
+            return quote_viewMenu();
+            selectchoice = false;
+            break;
+        }
+        case '2':
+        {
+            if (randomQuote.size() == 0)
+            {
+                showMessage("No quotes to delete", "04", false);
+                return quote_viewMenu();
+                selectchoice = false;
+                break;
+            }
+            while (true)
+            {
+                erasePrint(0, 75, 9, 15);
+                gotoxy(38, 12);
+                std::cout << "Enter Quote number: ";
+                int n;
+                std::cin >> n;
+                if (n <= 0 || n > randomQuote.size())
+                {
+                    gotoxy(48, 14);
+                    std::cout << "Quote not found";
+                }
+                else
+                {
+                    if (n == randomQuote.size() - 1)
+                        randomQuote.pop_back();
+                    else if (n == 1)
+                        randomQuote.erase(randomQuote.begin());
+                    else
+                        randomQuote.erase(randomQuote.begin() + n - 1);
+                    gotoxy(48, 14);
+                    std::cout << "Quote deleted successfully";
+                }
+                std::cin.ignore();
+                break;
+            }
+            return quote_viewMenu();
+            selectchoice = false;
+            break;
+        }
+        case '3':
+        {
+            selectchoice = false;
+            break;
+        }
+        default:
+        {
+            break;
+        }
+        }
+    }
+    std::string line;
+    std::ofstream fquote("files/Quotes/quotes.txt", std::ios::trunc);
+    if (!randomQuote.empty())
+        for (int i = 0; i < randomQuote.size(); i++)
+        {
+            line = randomQuote.at(i);
+            line = incrypt(line);
+            fquote << line << std::endl;
+        }
+
+    fquote.close();
+    return;
+}
+void Quotes::quote_menu()
+{
+    {
+        std::ifstream fquote("files/Quotes/quotes.txt", std::ios::trunc);
+        if (fquote.is_open())
+        {
+            randomQuote.clear();
+            std::string tempquote;
+            while (!fquote.eof())
+            {
+                std::getline(fquote, tempquote);
+                tempquote = decrypt(tempquote);
+                if (!tempquote.empty())
+                    randomQuote.push_back(tempquote);
+            }
+            fquote.close();
+        }
+    }
+    char choice;
+    bool selectchoice = true;
+    system("cls");
+    notebook();
+    gotoxy(50, 6);
+    std::cout << "Quotes Menu";
+    gotoxy(49, 7);
+    std::cout << "+===========+";
+    gotoxy(48, 9);
+    std::cout << "1.Change quote mode";
+    gotoxy(48, 10);
+    std::cout << "2.View quotes";
+    gotoxy(48, 11);
+    std::cout << "3.Return main menu";
+    gotoxy(30, 13);
+    std::cout << "Select your option: ";
+    while (selectchoice)
+    {
+        gotoxy(52, 13);
+        choice = getch();
+        switch (choice)
+        {
+        case '1':
+        {
+            changeQuoteMode();
+            selectchoice = false;
+            break;
+        }
+        case '2':
+        {
+            quote_viewMenu();
+            selectchoice = false;
+            break;
+        }
+        case '3':
+        {
+            return main_menu();
+            selectchoice = false;
+            break;
+        }
+        default:
+            break;
+        }
+    }
+    return quote_menu();
+}
 void main_menu()
 {
-    ShowConsoleCursor(true);
+    showCursor(true);
     Privacy p;
     system("cls");
     system("color 07");
@@ -612,7 +983,8 @@ void main_menu()
     {
         gotoxy(49, 17);
         std::cout << "Quote menu";
-
+        Quotes q;
+        q.quote_menu();
         break;
     }
 
